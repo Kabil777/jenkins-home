bdc9cc2549bab20a8a12924dd76cba9dc7fab04c
Kabil_Arch <kabilmuthu777@gmail.com>
2025-10-03 09:12:00 +0530

    Create job-dsl.groovy

---
+++ job-dsl.groovy
@@ -0,0 +1,19 @@
+@Grab('org.yaml:snakeyaml:1.30')
+import org.yaml.snakeyaml.Yaml
+
+// Load repository.yaml
+def yaml = new Yaml()
+def reposFile = new File("${WORKSPACE}/jenkins-configs/repository.yaml")
+def config = yaml.load(reposFile.text)
+
+config.repos.each { repo ->
+    pipelineJob("${repo.name}-pipeline") {
+        description("Pipeline for ${repo.name}")
+        definition {
+            cps {
+                // load the right Jenkinsfile template based on type
+                script(readFileFromWorkspace("jenkins-configs/piplines/${repo.type}.Jenkinsfile"))
+            }
+        }
+    }
+}
