09e34a3c83eee0580d959c01c642fed1454839a6
Kabil777 <kabilmuthu777@gmail.com>
2025-10-03 15:29:40 +0530

    changes in dsl job

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,22 +1,30 @@
 import org.yaml.snakeyaml.Yaml
 import jenkins.model.*
-import org.jenkinsci.plugins.scriptsecurity.scripts.*
+import javaposse.jobdsl.dsl.*
 
 def yaml = new Yaml()
 def config = yaml.load(readFileFromWorkspace("repository.yaml"))
 
 config.repos.each { repo ->
-    pipelineJob("${repo.name}-pipeline") {
-        description("Pipeline for ${repo.name}")
-       parameters {
-        stringParam('REPO_URL', repo.url, 'Repository URL')
-        stringParam('IMAGE_NAME', repo.image?.name ?: '', 'Docker image name')
-        stringParam('IMAGE_TAG', repo.image?.tag ?: '', 'Docker image tag')
-      }
-       definition {
-            cps {
-                script(readFileFromWorkspace("pipelines/${repo.type}.Jenkinsfile"))
+    def jobName = "${repo.name}-pipeline"
+    def jobExists = Jenkins.instance.getItem(jobName) != null
+
+    if (repo.update || !jobExists) {
+        pipelineJob(jobName) {
+            description("Pipeline for ${repo.name}")
+            parameters {
+                stringParam('REPO_URL', repo.url, 'Repository URL')
+                stringParam('IMAGE_NAME', repo.image?.name ?: '', 'Docker image name')
+                stringParam('IMAGE_TAG', repo.image?.tag ?: '', 'Docker image tag')
+            }
+            definition {
+                cps {
+                    script(readFileFromWorkspace("pipelines/${repo.type}.Jenkinsfile"))
+                }
             }
         }
+    } else {
+        println "Skipping update for ${jobName} (update flag = false)"
     }
 }
+
