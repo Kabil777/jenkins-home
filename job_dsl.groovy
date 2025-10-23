d039754eb1b1eefb3b09d86ad173c57719d2e336
Kabil777 <kabilmuthu777@gmail.com>
2025-10-23 17:29:22 +0530

    Added template configs

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -14,7 +14,7 @@ config.repos.each { repo ->
             description("Pipeline for ${repo.name}")
             parameters {
                 stringParam('NAME', repo.name, 'Repository name')
-                stringParam('PORT', repo.image?.port ?: 80, 'Deployment port')
+                stringParam('PORT', repo.image?.port ?: '80', 'Deployment port')
                 stringParam('REPO_URL', repo.url, 'Repository URL')
                 stringParam('IMAGE_NAME', repo.image?.name ?: '', 'Docker image name')
                 stringParam('IMAGE_TAG', repo.image?.tag ?: '', 'Docker image tag')
